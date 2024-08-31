a3b1041972e82d787864d0fca1f33aa48e374589
Your Name <you@example.com>
2024-08-31 13:04:50 +0100

    add routing module

---
+++ prokick-web/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // Ajoutez d'autres routes ici
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
