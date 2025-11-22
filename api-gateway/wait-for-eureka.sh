b142385dc99a308383cff1843ca8ea9069294e84
MedHachem <hechemmohamed9@gmail.com>
2025-11-22 14:26:33 +0100

    add unversionned files

---
+++ api-gateway/wait-for-eureka.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# wait-for-eureka.sh
+# Usage: ./wait-for-eureka.sh host port command
+
+set -e
+
+host="$1"
+port="$2"
+shift 2
+cmd="$@"
+
+echo "Waiting for $host:$port to be available..."
+
+while ! nc -z "$host" "$port"; do
+  echo "Waiting for $host:$port..."
+  sleep 2
+done
+
+echo "$host:$port is available. Starting command: $cmd"
+exec $cmd
